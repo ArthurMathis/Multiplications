d66481364608379afda1b9af4eee77580da3b1c0
ArthurMathis <mathis.arthur68@gmail.com>
2023-12-04 19:28:35 +0100

    Nug de la propriété value

--- tablemultcontroller.js
+++ tablemultcontroller.js
@@ -1,30 +1,32 @@
 const bResult = document.getElementById('bresult');
 const bVerif = document.getElementById('bverify');
-const selecteur = docuement.getElementById('selectnum');
-
-function update(){
-
-}
+const select = document.getElementById('selectnum');
 
 function init(){
-    let table = new Table(selecteur.value);
-    selecteur.addEventListener('change', update(), false);
-    bResult.addEventListener('click', () => {
-        // à compléter
-    });
-    bVerif.addEventListener('click', () => {
-        // à compléter
-    });
+    let table = new Table(select.value);
+
+    select.addEventListener('change', update, false);
+    bResult.addEventListener('click', () => { /* à compléter */ });
+    bVerif.addEventListener('click', () => { /* à compléter */ });
+}
+
+function update(){
+    const n = select.value;
+    table.changeCurrentTableNumber(n);
 }
 
 function remove(){
-    bResult.removeEventListener('click', () => {
-        // à compléter
-    });
-    bVerif.removeEventListener('click', () => {
-        // à compléter
-    });
+    bResult.removeEventListener('click', () => { /* à compléter */ });
+    bVerif.removeEventListener('click', () => { /* à compléter */ });
 }
 
 window.addEventListener("load", init, false);
 window.removeEventListener("unload", remove, false);
+
+let test = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+let tableTest = new Table(2);
+let resTest = tableTest.valuesVerification(test);
+
+resTest.forEach((c) => {
+    console.log(c);
+});
--- tablemultdata.js
+++ tablemultdata.js
@@ -1,8 +1,9 @@
 class Table{
-    constructor(){
-        this.currentTableNumber = 0;
-    }
     constructor(n){
+        this.currentTableNumber = n;
+    }
+
+    changeCurrentTableNumber(n){
         this.currentTableNumber = n;
     }
 
@@ -13,13 +14,13 @@ class Table{
     tableValues(){
         let c = [];
         for(let i = 1; i <= 10; i++){
-            res.push(i * this.tableNumber());
+            c.push(i * this.tableNumber());
         }
         return c;
     }
 
     valuesVerification(values){
-        const c = tableValues();
+        const c = this.tableValues();
         let res = [];
         for(let i = 0; i <= 10; i++){
             res.push(c[i] === values[i] ? true : false);
