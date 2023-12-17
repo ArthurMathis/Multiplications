904fd041ec52a1885e75b583de4f09e7b1d123c9
ArthurMathis <mathis.arthur68@gmail.com>
2023-12-17 16:00:44 +0100

    Ajout de la fonction displayResults

--- tablemultview.js
+++ tablemultview.js
@@ -1,4 +1,5 @@
 let table = document.querySelectorAll('.num');
+const inputs = document.querySelectorAll('input.result');
 
 function writeTableNumber(num){
     table.forEach(element => {
@@ -20,6 +21,14 @@ function onChangeTableNumber(){
     changeTableNumber(selectedTableNumber());
     selectValue(tableNumber());
     writeTableNumber(tableNumber());
+}
+
+function displayResults(results){
+    let i = 0;
+    inputs.forEach(element => {
+        element.value = results[i];
+        i++;
+    });
 }
     
 select.addEventListener('change', onChangeTableNumber);
